ca772ddee5982efd97831474863dc31f25a1e1e7
option352 <keisuke.miyagawa@gmail.com>
2018-01-10 17:51:08 +0900

    シーン切り替え

--- js/sample/main.js
+++ js/sample/main.js
@@ -5,6 +5,7 @@ window.onload = function()
   var game = new Game(320, 320);
   game.fps = 24;
   game.preload('./img/enchant4.png');
+  /*
   game.onload = function()
   {
     var kuma = new Sprite(32, 32);
@@ -26,6 +27,55 @@ window.onload = function()
       speed = e.x > kuma.x ? 1 : -1;
       kuma.scaleX = speed;
     });
+  }
+  */
+  
+  game.onload = function()
+  {
+    var createTitleScene = function()
+    {
+      var scene = new Scene();
+      var label = new Label("title scene");
+      scene.addChild(label);
+      scene.backgroundColor = "rgba(255, 230, 0, 1)";
+      scene.addEventListener(Event.TOUCH_START, function(e)
+      {
+        game.replaceScene(createGameScene());
+      });
+      
+      return scene;
+    }
+    
+    var createGameScene = function()
+    {
+      var scene = new Scene();
+      var label = new Label("game scene");
+      scene.addChild(label);
+      scene.backgroundColor = "rgba(255, 200, 0, 1)";
+      scene.addEventListener(Event.TOUCH_START, function(e)
+      {
+        game.pushScene(createGameoverScene());
+      });
+      return scene;
+    }
+    
+    var createGameoverScene = function()
+    {
+      var scene = new Scene();
+      var label = new Label("game over scene");
+      label.x = 0;
+      label.y = 20;
+      scene.addChild(label);
+      scene.backgroundColor = "rgba(0, 0, 255, 0.5)";
+      scene.addEventListener(Event.TOUCH_START, function(e)
+      {
+        //game.popScene();
+        game.replaceScene(createTitleScene());
+      });
+      return scene;
+    }
+
+    game.replaceScene(createTitleScene());
   }
   
   game.start();
