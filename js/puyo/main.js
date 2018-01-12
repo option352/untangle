39d938280e38800a970f8cea8a61076fbdc0fb10
option352 <keisuke.miyagawa@gmail.com>
2018-01-12 16:57:42 +0900

    消去処理

--- js/puyo/main.js
+++ js/puyo/main.js
@@ -26,18 +26,31 @@ window.onload = function()
         if ( j == 0 || j == MAX_COL - 1 || i == MAX_ROW - 1)
           temp_array[j] = 0;
         else
-          temp_array[j] = -1;
+          //temp_array[j] = -1;
+          temp_array[j] = Math.floor(Math.random() * 4 + 1);
       }
       field[i] = temp_array;
     }
     
     map.image = game.assets[PUYOS_IMG];
     map.loadData(field);
-    //scene.addChild(map);
+    scene.addChild(map);
     
-    var pair = createPair(game, map, field);
-    scene.addChild(pair);
+    //var pair = createPair(game, map, field);
+    //scene.addChild(pair);
     
+    scene.addEventListener(Event.TOUCH_START, function()
+    {
+      var target_row = Math.floor(Math.random() * (MAX_ROW - 1) + 1);
+      var target_col = Math.floor(Math.random() * (MAX_COL - 2) + 1);
+      var count = countPuyos(target_row, target_col, field);
+      console.log(target_row, target_col, count);
+      if (count >= 4)
+      {
+        deletePuyos(target_row, target_col, field)
+        map.loadData(field);
+      }
+    });
   }
   
   
@@ -45,83 +58,118 @@ window.onload = function()
   game.start();
 }
 
-    function createPuyo(game)
+function createPuyo(game)
+{
+  var puyo = new Sprite(CELL_SIZE, CELL_SIZE);
+  puyo.image = game.assets[PUYOS_IMG];
+  puyo.frame = Math.floor(Math.random() * 4 + 1);
+  puyo.moveTo(0, 0);
+  return puyo;
+}
+
+function createPair(game, map, field)
+{
+  var pair = new Group();
+  var p0 = createPuyo(game);
+  var p1 = createPuyo(game);
+  var forms = [[0, -CELL_SIZE], [CELL_SIZE, 0], [0, CELL_SIZE], [-CELL_SIZE, 0]];
+  var formNum = 0;
+  var inputRightCount = 0;
+  var inputLeftCount = 0;
+  var inputACount = 0;
+  pair.isFall = true;
+  pair.addChild(p0);
+  pair.addChild(p1);
+  p0.y = -CELL_SIZE;
+  pair.moveTo(CELL_SIZE*3, CELL_SIZE);
+  
+  pair.addEventListener(Event.ENTER_FRAME, function()
+  {
+    inputRightCount = game.input.right ? inputRightCount + 1 : 0;
+    inputLeftCount = game.input.left ? inputLeftCount + 1 : 0;
+    inputACount = game.input.a ? inputACount + 1 : 0;
+    
+    if(inputACount == 1)
     {
-      var puyo = new Sprite(CELL_SIZE, CELL_SIZE);
-      puyo.img = game.assets[PUYOS_IMG];
-      puyo.frame = Math.floor(Math.random() * 4 + 1);
-      puyo.moveTo(0, 0);
-      return puyo;
+      var newFormNum = (formNum + 1 ) % 4;
+      var newX = forms[newFormNum][0];
+      var newY = forms[newFormNum][1];
+      if (!map.hitTest(this.x + newX, this.y + newY))
+      {
+        formNum = newFormNum;
+        p0.moveTo(newX, newY);
+      }
     }
     
-    function createPair(game, map, field)
+    var newX = 0;
+    if (inputRightCount == 1)
     {
-      var pair = new Group();
-      var p0 = createPuyo(game);
-      var p1 = createPuyo(game);
-      var forms = [[0, -CELL_SIZE], [CELL_SIZE, 0], [0, CELL_SIZE], [-CELL_SIZE, 0]];
-      var formNum = 0;
-      var inputRightCount = 0;
-      var inputLeftCount = 0;
-      var inputACount = 0;
-      pair.isFall = true;
-      pair.addChild(p0);
-      pair.addChild(p1);
-      p0.y = -CELL_SIZE;
-      pair.moveTo(CELL_SIZE*3, CELL_SIZE);
-      
-      pair.addEventListener(Event.ENTER_FRAME, function()
-      {
-        inputRightCount = game.input.right ? inputRightCount + 1 : 0;
-        inputLeftCount = game.input.left ? inputLeftCount + 1 : 0;
-        inputACount = game.input.a ? inputACount + 1 : 0;
-        
-        if(inputACount == 1)
-        {
-          var newFormNum = (formNum + 1 ) % 4;
-          var newX = forms[newFormNum][0];
-          var newY = forms[newFormNum][1];
-          if (!map.hitTest(this.x + newX, this.y + newY))
-          {
-            formNum = newFormNum;
-            p0.moveTo(newX, newY);
-          }
-        }
-        
-        var newX = 0;
-        if (inputRightCount == 1)
-        {
-          newX = formNum == 1 ? p0.x + CELL_SIZE : p1.x + CELL_SIZE;
-        }
-        if (inputLeftCount == 1)
-        {
-          newX = formNum == 3 ? p0.x - CELL_SIZE : p1.x - CELL_SIZE;
-        }
-        if (!map.hitTest(this.x + newX, this.y + p0.y) && map.hitTest(this.x + newX, this.y + p1.y))
-        {
-          this.x = this.x + (newX ? (newX >= 0 ? 1 : -1) : 0) * CELL_SIZE;
-        }
-        
-        newY = formNum==2 ? p0.y + CELL_SIZE : p1.y + CELL_SIZE;
-        
-        var vy = Math.floor(game.input.down ? game.fps / 10 : game.fps / 1);
-        if (game.frame % vy == 0)
-        {
-          if (!map.hitTest(this.x + p0.x, this.y + newY) && !map.hitTest(this.x + p1.x, this.y + newY))
-          {
-            this.y += CELL_SIZE;
-          }
-          else
-          {
-            field[(this.y+p0.y)/CELL_SIZE][(this.y+p0.x)/CELL_SIZE] = p0.frame;
-            field[(this.y+p1.y)/CELL_SIZE][(this.y+p1.x)/CELL_SIZE] = p1.frame;
-            pair.isFall = false;
-          }
-        }
-        
-      });
-      
-      console.log(pair)
-      console.log(p0);
-      return pair;
+      newX = formNum == 1 ? p0.x + CELL_SIZE : p1.x + CELL_SIZE;
     }
+    if (inputLeftCount == 1)
+    {
+      newX = formNum == 3 ? p0.x - CELL_SIZE : p1.x - CELL_SIZE;
+    }
+    if (!map.hitTest(this.x + newX, this.y + p0.y) && map.hitTest(this.x + newX, this.y + p1.y))
+    {
+      this.x = this.x + (newX ? (newX >= 0 ? 1 : -1) : 0) * CELL_SIZE;
+    }
+    
+    newY = formNum==2 ? p0.y + CELL_SIZE : p1.y + CELL_SIZE;
+    
+    var vy = Math.floor(game.input.down ? game.fps / 10 : game.fps / 1);
+    if (game.frame % vy == 0)
+    {
+      if (!map.hitTest(this.x + p0.x, this.y + newY) && !map.hitTest(this.x + p1.x, this.y + newY))
+      {
+        this.y += CELL_SIZE;
+      }
+      else
+      {
+        field[(this.y+p0.y)/CELL_SIZE][(this.y+p0.x)/CELL_SIZE] = p0.frame;
+        field[(this.y+p1.y)/CELL_SIZE][(this.y+p1.x)/CELL_SIZE] = p1.frame;
+        pair.isFall = false;
+      }
+    }
+    
+  });
+  
+  return pair;
+}
+
+function countPuyos(row, col, field)
+{
+  var c = field[row][col];
+  if(c <= 0)
+    return 0;
+  
+  var n = 1;
+  field[row][col] = -1;
+  if (row - 1 >= 2 && field[row - 1][col] == c)
+    n += countPuyos(row - 1, col, field);
+  if (row + 1 <= MAX_ROW - 2 && field[row + 1][col] == c)
+    n += countPuyos(row + 1, col, field);
+  if (col - 1 >= 2 && field[row][col - 1] == c)
+    n += countPuyos(row, col - 1, field);
+  if (col - 1 >= 2 && field[row][col + 1] == c)
+    n += countPuyos(row, col + 1, field);
+  field[row][col] = c;
+  return n;
+}
+
+function deletePuyos(row, col, field)
+{
+  var c = field[row][col];
+  field[row][col] = -1;
+  if (row - 1 >= 2 && field[row - 1][col] == c)
+    deletePuyos(row - 1, col, field);
+  if (row + 1 <= MAX_ROW - 2 && field[row + 1][col] == c)
+    deletePuyos(row + 1, col, field);
+  if (col - 1 >= 2 && field[row][col - 1] == c)
+    deletePuyos(row, col - 1, field);
+  if (col - 1 >= 2 && field[row][col + 1] == c)
+    deletePuyos(row, col + 1, field);
+}
+
+
+
